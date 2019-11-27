0269f636fbbac5ed72081e5d703d4e38cf2b8676
dougantrhoupa <douglas@houpa.app>
2019-11-27 12:45:43 -0300

    criação de posts, listagem de posts, login

---
+++ src/app/controllers/PostDetailController.js
@@ -0,0 +1,8 @@
+import Post from "../models/Post";
+import PostExercises from "../models/Post";
+
+class PostDetailController {
+  index(req, res) {}
+}
+
+export default new PostDetailController();
